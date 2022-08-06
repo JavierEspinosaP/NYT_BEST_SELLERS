316f94a19876400ddd0684aa66a9d6c6750a2993
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-08-06 16:20:19 +0200

    add/selectors

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 
+//Selectors
+
+
+
+
 async function getBook () {
     let response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${key.api_key}`)
     let data = await response.json()
---
+++ scripts/selectors.js
@@ -0,0 +1,58 @@
+//CONTAINER
+
+const container = document.querySelector('.container');
+
+//H3
+const h3list1 = document.getElementById('h3list1');
+const h3list2 = document.getElementById('h3list2');
+const h3list3 = document.getElementById('h3list3');
+const h3list4 = document.getElementById('h3list4');
+const h3list5 = document.getElementById('h3list5');
+const h3list6 = document.getElementById('h3list6');
+const h3list7 = document.getElementById('h3list7');
+const h3list8 = document.getElementById('h3list8');
+const h3list9 = document.getElementById('h3list9');
+const h3list10 = document.getElementById('h3list10');
+const h3list11 = document.getElementById('h3list11');
+const h3list12 = document.getElementById('h3list12');
+
+//LINES
+
+const list1Oldest = document.getElementById('list1Oldest');
+const list2Oldest = document.getElementById('list2Oldest');
+const list3Oldest = document.getElementById('list3Oldest');
+const list4Oldest = document.getElementById('list4Oldest');
+const list5Oldest = document.getElementById('list5Oldest');
+const list6Oldest = document.getElementById('list6Oldest');
+const list7Oldest = document.getElementById('list7Oldest');
+const list8Oldest = document.getElementById('list8Oldest');
+const list9Oldest = document.getElementById('list9Oldest');
+const list10Oldest = document.getElementById('list10Oldest');
+const list11Oldest = document.getElementById('list11Oldest');
+const list12Oldest = document.getElementById('list12Oldest');
+
+const list1Newest = document.getElementById('list1Newest');
+const list2Newest = document.getElementById('list2Newest');
+const list3Newest = document.getElementById('list3Newest');
+const list4Newest = document.getElementById('list4Newest');
+const list5Newest = document.getElementById('list5Newest');
+const list6Newest = document.getElementById('list6Newest');
+const list7Newest = document.getElementById('list7Newest');
+const list8Newest = document.getElementById('list8Newest');
+const list9Newest = document.getElementById('list9Newest');
+const list10Newest = document.getElementById('list10Newest');
+const list11Newest = document.getElementById('list11Newest');
+const list12Newest = document.getElementById('list12Newest');
+
+const list1Updated = document.getElementById('list1Updated');
+const list2Updated = document.getElementById('list2Updated');
+const list3Updated = document.getElementById('list3Updated');
+const list4Updated = document.getElementById('list4Updated');
+const list5Updated = document.getElementById('list5Updated');
+const list6Updated = document.getElementById('list6Updated');
+const list7Updated = document.getElementById('list7Updated');
+const list8Updated = document.getElementById('list8Updated');
+const list9Updated = document.getElementById('list9Updated');
+const list10Updated = document.getElementById('list10Updated');
+const list11Updated = document.getElementById('list11Updated');
+const list12Updated = document.getElementById('list12Updated');
