520a135e56a5ca2ad5fcd8cf4709cb37f6496d15
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-08-06 18:13:42 +0200

    add/getLists_logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,19 @@
 
-//Selectors
 
+async function getLists () {
+    let responselist = await fetch(`https://api.nytimes.com/svc/books/v3/lists/names?api-key=${key.api_key}`)
+    let data = await responselist.json()
+    let lists = data.results.map((list) => {
+        return list.list_name_encoded
+    })
+    let responseOneList = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${lists[0]}.json?api-key=ccKqbSWo3G6EUHxA4MQOpZJOVTa8P3FL`)
+    let dataOneList = await responseOneList.json()
+    let h3List = dataOneList.results.display_name
+    //Para sacar el "Newest" Iterar "books" y pushear el valor "weeks_on_list" y "title" en un array de objetos, ordenarlo de mayor a menor y luego
+    //extraer el valor "title" para pintarlo en el DOM, para el "Oldest" lo contrario.
+    console.log(h3List);
 
-
-
-async function getBook () {
-    let response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${key.api_key}`)
-    let data = await response.json()
-    let image = data.results.books[0].book_image
-    console.log(image);
-    let div = document.createElement('section');
-    document.body.appendChild(div);
-        div.innerHTML=`
-        <img src="${image}"<br>
-`
 }
+
+getLists ()
+
