28e8100c7446bffb21ec82f1d7048bdb34f57fc1
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-08-28 23:05:11 +0200

    commit con gitignore vacio

---
+++ scripts/config.js
@@ -0,0 +1,18 @@
+const key = {
+    api_key: "ccKqbSWo3G6EUHxA4MQOpZJOVTa8P3FL"
+}
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAhK10tJ9zIJyyzRcP7RewQqkGewmYfOM",
+    authDomain: "nyt-best-sellers-10ebb.firebaseapp.com",
+    projectId: "nyt-best-sellers-10ebb",
+    storageBucket: "nyt-best-sellers-10ebb.appspot.com",
+    messagingSenderId: "949461611754",
+    appId: "1:949461611754:web:0f3141fdc7bbe9b3b25393",
+    measurementId: "G-Q8T3T68WSF"
+  };
+
+
+  
+  
